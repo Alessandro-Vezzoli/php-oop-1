224e3661b1310717530e74f4dba09ae34296976f
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2024-03-17 13:18:05 +0100

    add static method and proprierty method

--- index.php
+++ index.php
@@ -4,12 +4,24 @@ class Movie {
     public $title;
     public $genre = [];
     public $releaseYear;
+    public static $counter = 0;
+     
     
      
     public function __construct($title, $genres, $releaseYear) {
         $this->title = $title;
         $this->genre = $genres;
         $this->releaseYear = $releaseYear;
+        self::$counter++;
+    }
+
+    public function displayCounter() {
+        echo "Number of movies created: " . self::$counter . "<br>";
+    }
+
+   
+     public static function displayStaticInfo() {
+        echo "This is a static method.<br>";
     }
 
      public function displayInfo() {
@@ -23,6 +35,9 @@ class Movie {
 $movie1 = new Movie("Inception", ["Sci-Fi", "Action"], 2010);
 $movie2 = new Movie("The Shawshank Redemption", ["Drama"], 1994);
 
+$movie1->displayCounter();
+
+Movie::displayStaticInfo();
 
 $movie1->displayInfo();
 $movie2->displayInfo();
